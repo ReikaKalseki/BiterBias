55751be9b077826397b0a020f6d4e8aafe42478e
Reika <reikasminecraft@gmail.com>
2021-04-23 17:51:19 -0400

    Update 11 - 1.1 Update

--- settings.lua
+++ settings.lua
@@ -23,7 +23,7 @@ data:extend({
             setting_type = "startup",
             default_value = 0.33,
 			minimum_value = 0.01,
-			maximum_value = 0.99,
+			maximum_value = 1.0,
             order = "r",
 			localised_name = "Biter bias factor",
 			localised_description = "Change this to modify the bias factor, that is, the Biter/Spitter spawn ratio. 1.0 is vanilla settings; at 0.5, 50% of spitter spawns become biter spawns. At 0.25, 75% do.",
